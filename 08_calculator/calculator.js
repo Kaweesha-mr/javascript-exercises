62242bc95875d8931d72c8106ddde62da22d1f56
Kaweesha-mr <kaweesha.mr@gmail.com>
2023-03-12 13:10:40 +0530

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -46,29 +46,20 @@ const power = function(N1,N2) {
 
 const factorial = function(number) {
 
+  let fac =1;
+  for(let i =1; i< number; i++){
 
-  let final =1;
-  
-  if(number ==1){
 
-    return 1;
-  }
+    if(number == 1){
 
-  let count = number;
-  for(let i =0; i<number;i++){
-
-    final = final * (count * count-1);
-    
-    count--;
-
-    if(count == 1){
-
-      return final;
+      return 1;
     }
+
+    fac = fac * (i+1);
+
   }
 
-  return final;
-	
+  return fac;
 };
 
 // Do not edit below this line
